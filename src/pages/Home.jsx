6fa055d1d911a6cf3f78f8f17a8a7e1385a8d9d5
choiruladamm <choiruladamm@gmail.com>
2023-03-22 17:56:18 +0700

    Dojo Blogs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(21);
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button
-        onClick={() => {
-          setName("John");
-          setAge(40);
-        }}
-      >
-        Click me
-      </button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
