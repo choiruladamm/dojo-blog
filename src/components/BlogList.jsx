7abf714bdac74e616e1f8a7a74699f714683f750
choiruladamm <choiruladamm@gmail.com>
2023-03-22 18:41:00 +0700

    Props

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <p>{blog.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import BlogList from "../components/BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -9,13 +10,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-          <p>{blog.body}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 };
