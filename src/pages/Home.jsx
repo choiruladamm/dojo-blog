def4aaa507a5a904fea0e5a119cbd57e6eeec9b5
choiruladamm <choiruladamm@gmail.com>
2023-03-22 22:15:41 +0700

    Using JSON Server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
     { title: "Welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
     { title: "Web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
   ]);
-  const [name, setName] = useState("Adam");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -15,15 +14,12 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log('useEffect Runnign')
-    console.log(name);
-  }, [name]);
+    console.log("useEffect Running");
+  }, []);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName("Axell")}>Change Name</button>
-      <p>{name}</p>
     </div>
   );
 };
