d2082f515e0a55cc5acbfbd4c99b0fca14038b13
choiruladamm <choiruladamm@gmail.com>
2023-03-25 11:33:15 +0700

    The React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const title = "Welcome to the new blog";
-
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
