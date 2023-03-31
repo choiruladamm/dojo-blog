e0fff1aa80fbf56468013fbd332895a8bfeeb2ad
choiruladamm <choiruladamm@gmail.com>
2023-03-31 22:35:40 +0700

    Submit Events

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -5,10 +5,16 @@ const Create = () => {
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add New Blogs</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
