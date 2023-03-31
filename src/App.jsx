c0d48476f0f030f93aca80da40b474c259f735e7
choiruladamm <choiruladamm@gmail.com>
2023-03-31 21:08:40 +0700

    Route Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./pages/Create";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
