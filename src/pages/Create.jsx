d7ac64d2928fa67371e08189ce6d4982e84ba782
choiruladamm <choiruladamm@gmail.com>
2023-07-15 16:36:01 +0700

    Dojo Blogs

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -8,7 +8,14 @@ const Create = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog succesfully added");
+    });
   };
 
   return (
